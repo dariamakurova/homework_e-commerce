982a6eef631690d266c83d522e99a8011b554ca6
Daria_Makurova <dariamakurova@gmail.com>
2025-10-24 22:52:34 +0300

    open_json.pyjson: open and create objects

--- src/open_json.py
+++ src/open_json.py
@@ -1,15 +1,44 @@
 import json
 import os
 
+from src.category import Category
+from src.product import Product
 
-def open_json(file):
+
+def open_json(path):
     """ Получение данных из json файла и создание объектов класса """
 
+    with open(path,'r', encoding="UTF-8") as file:
+        products = json.load(file)
+    return products
 
-    products = json.load(file)
+def create_objects_from_json(data):
+
+    categories = []
+    for category in data:
+        products = []
+        for product in category["products"]:
+            products.append(Product(**product))
+        category["products"] = products
+        categories.append(Category(**category))
+    return categories
+
+if __name__ == "__main__":
+
+    path = os.path.join((os.path.dirname(os.path.dirname(__file__))), "data", "products.json")
+    data_ = open_json(path)
+    categories_ = create_objects_from_json(data_)
+    print(categories_)
+
+    print(categories_[0].name)
+    print(categories_[0].description)
+
+
+
 
 
 
 if __name__ == "__main__":
 
     path = os.path.join((os.path.dirname(os.path.dirname(__file__))), "data", "products.json")
+    open_json(path)
