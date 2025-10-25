ef08aac614dbe07b8c2e6eca541fe5b90deb95df
Daria_Makurova <dariamakurova@gmail.com>
2025-10-25 12:07:23 +0300

    test_category improved

--- tests/test_category.py
+++ tests/test_category.py
@@ -10,5 +10,5 @@ def test_category_init(category_1, category_2):
     assert category_2.description == "Category_of_products_2"
     assert category_2.products == ["product_3", "product_4"]
 
-    assert Category.number_of_categories == 2
-    assert Category.number_of_products == 4
+    assert Category.category_count == 2
+    assert Category.product_count == 4
