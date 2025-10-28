dbbcd5066d53f9e535d3e855aef6e7c3d1c04bef
Daria_Makurova <dariamakurova@gmail.com>
2025-10-29 00:16:30 +0300

    14_2_main.py def add_product

--- src/category.py
+++ src/category.py
@@ -11,6 +11,10 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(self.products)
+
+
+    def add_product(self, product):
+        self.__products = product
