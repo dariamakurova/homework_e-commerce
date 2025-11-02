23f9aad1a3c066ce5cfe6bbac2b7737976db748f
Daria_Makurova <dariamakurova@gmail.com>
2025-11-03 01:30:40 +0300

    category_iterator with tests

---
+++ src/category_iterator.py
@@ -0,0 +1,21 @@
+
+class CategoryIterator:
+    """ Класс-итератор для перебора товаров в одной категории"""
+
+    def __init__(self, category):
+        self.category = category
+        self.index = 0
+
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+
+    def __next__(self):
+        if self.index < len(self.category.products):
+            product = self.category.products[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
