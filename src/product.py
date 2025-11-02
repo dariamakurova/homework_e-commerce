43e9c1bb1c9a91a7286cc9824de8edbf74e7ff9d
Daria_Makurova <dariamakurova@gmail.com>
2025-11-03 00:14:42 +0300

    product __add__

--- src/product.py
+++ src/product.py
@@ -20,6 +20,11 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
 
+
+    def __add__(self, other):
+        return self.__price * self.quantity + other.__price * other.quantity
+
+
     @classmethod
     def new_product(cls, new_product):
         for product in cls._products:
